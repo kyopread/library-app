93638a4aa199f9ae3ecf64627d7c1314c8968cd4
kyopread <djflsthskan2@gmail.com>
2022-10-31 00:17:56 +0900

    test: 책 관련 기능 테스트 작성

---
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -0,0 +1,64 @@
+package com.group.libraryapp.service.book
+
+import com.group.libraryapp.domain.book.Book
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.dto.book.request.BookLoanRequest
+import com.group.libraryapp.dto.book.request.BookRequest
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTest @Autowired constructor(
+    private val bookService: BookService,
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+) {
+
+    @AfterEach
+    fun clean() {
+        bookRepository.deleteAll()
+    }
+
+    @Test
+    @DisplayName("책 등록이 정상 동작한다.")
+    fun saveBookTest() {
+        //given
+        val request = BookRequest("이상한 나라의 엘리스")
+
+        //when
+        bookService.saveBook(request)
+
+        //then
+        val books = bookRepository.findAll()
+        assertThat(books).hasSize(1)
+        assertThat(books[0].name).isEqualTo("이상한 나라의 엘리스")
+    }
+
+    @Test
+    @DisplayName("책 대출이 정상 동작한다.")
+    fun loanBookTest() {
+        //given
+        bookRepository.save(Book("이상한 나라의 엘리스"))
+        val savedUser = userRepository.save(User("홍길동", null))
+        val request = BookLoanRequest("홍길동", "이상한 나라의 엘리스")
+
+        //when
+        bookService.loanBook(request)
+
+        //then
+        val results = userLoanHistoryRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].bookName).isEqualTo("이상한 나라의 엘리스")
+        assertThat(results[0].user.id).isEqualTo(savedUser.id)
+        assertThat(results[0].isReturn).isFalse
+    }
+
+}
